24ff81f5b2e6513fb969a195cddb4cb3ccdd26c1
Samuel Tesla <samuel.tesla@gmail.com>
2019-04-05 19:38:32 -0500

    don't blow up if previous_posecount has never been set

--- typeclasses/scripts/weekly_events.py
+++ typeclasses/scripts/weekly_events.py
@@ -299,7 +299,9 @@ class WeeklyEvents(RunDateMixin, Script):
             if (ob.posecount < min_poses and (ob.tags.get("rostercg")and ob.player_ob
                                               and not ob.player_ob.tags.get("staff_alt"))):
                 low_activity.append(ob)
-            ob.db.previous_posecount = ob.posecount
+            if ob.db.previous_posecount is None:
+                ob.db.previous_posecount = 0
+            ob.db.previous_posecount += ob.posecount
             ob.posecount = 0
         board = BBoard.objects.get(db_key__iexact="staff")
         table = EvTable("{wName{n", "{wNum Poses{n", border="cells", width=78)
